3c98ed4ba6c71a30ee46580a3f3aa54e16eb567c
SilentWrangler <thousandgoblins@gmail.com>
2023-04-24 20:07:34 +0300

    Подготовка к логике экспансии

--- world/constants.py
+++ world/constants.py
@@ -1,21 +1,22 @@
 from django.db import models
 
+
 class WORLD_GEN:
     WIDTH = 200
     HEIGHT = 150
-    MAP_SIZE = WIDTH*HEIGHT
+    MAP_SIZE = WIDTH * HEIGHT
     STARTING_CITY_FRACTION = 0.01
-    CITY_NUMBER = int(MAP_SIZE*STARTING_CITY_FRACTION)
+    CITY_NUMBER = int(MAP_SIZE * STARTING_CITY_FRACTION)
     CITY_SCORE = 6
     STARTING_SORROW_FRACTION = 0.01
-    INFECTED_CELLS = int(MAP_SIZE*STARTING_SORROW_FRACTION)
+    INFECTED_CELLS = int(MAP_SIZE * STARTING_SORROW_FRACTION)
     POPS_PER_CITY = 3
-    ERUPTIONS = int(MAP_SIZE/12)
+    ERUPTIONS = int(MAP_SIZE / 12)
     ERUPTION_POWER = 30
     FOREST_FRACTION = 0.5
-    FOREST_CELLS = int(MAP_SIZE*FOREST_FRACTION)
+    FOREST_CELLS = int(MAP_SIZE * FOREST_FRACTION)
     SWAMP_FRACTION = 0.4
-    SWAMP_CELLS = int(MAP_SIZE*SWAMP_FRACTION)
+    SWAMP_CELLS = int(MAP_SIZE * SWAMP_FRACTION)
 
 
 class RESOURCE_TYPE(models.TextChoices):
@@ -42,6 +43,7 @@ class MAIN_BIOME(models.TextChoices):
     PLAIN = 'PLN'
     DESERT = 'DSR'
 
+
 class BIOME_MOD(models.TextChoices):
     NONE = 'NON'
     FOREST = 'FRT'
@@ -62,16 +64,17 @@ class CITY_TYPE(models.TextChoices):
 
     __empty__ = 'No City'
 
-#Типы городов, создаваемые проектом постройки тайла
-#Гнездо Печали, очевидно, построить нельзя
-#Библиотека и Форт строятся отдельными проектами, работающими не от экономики, а от науки и военного дела
+
+# Типы городов, создаваемые проектом постройки тайла
+# Гнездо Печали, очевидно, построить нельзя
+# Библиотека и Форт строятся отдельными проектами, работающими не от экономики, а от науки и военного дела
 CIVILIAN_CITIES = [
     CITY_TYPE.GENERIC,
     CITY_TYPE.MANA,
     CITY_TYPE.FARM,
     CITY_TYPE.FACTORY,
     CITY_TYPE.MINE
-    ]
+]
 
 
 class POP_RACE(models.TextChoices):
@@ -82,21 +85,86 @@ class POP_RACE(models.TextChoices):
     GOBLIN = 'GOB'
     FEY = 'FEY'
 
+
 class POP_TAG_NAMES(models.TextChoices):
     GROWTH = 'GROWTH'
     FACTION = 'FACTION'
     SUPPORTED_CHARACTER = 'CHARACTER'
 
+
 class CELL_TAG_NAMES(models.TextChoices):
     NAME = 'NAME'
+    BOOST = 'BOOST'
 
 
 class UNIQUE_TAGS:
     ONE_PER_CELL = [CELL_TAG_NAMES.NAME]
-    ONE_PER_POP = [POP_TAG_NAMES.GROWTH, POP_TAG_NAMES.FACTION,POP_TAG_NAMES.SUPPORTED_CHARACTER]
+    ONE_PER_POP = [POP_TAG_NAMES.GROWTH, POP_TAG_NAMES.FACTION, POP_TAG_NAMES.SUPPORTED_CHARACTER]
 
 
+class BALANCE:
+    BASE_BIOME_FOOD = {
+        MAIN_BIOME.PLAIN: {
+            BIOME_MOD.NONE: 4,
+            BIOME_MOD.FOREST: 3,
+            BIOME_MOD.SWAMP: 3,
+            BIOME_MOD.HILLS: 2,
+            BIOME_MOD.MOUNTAINS: 1
+        },
+        MAIN_BIOME.DESERT: {
+            BIOME_MOD.NONE: 1,
+            BIOME_MOD.FOREST: 2,
+            BIOME_MOD.SWAMP: 3,
+            BIOME_MOD.HILLS: 1,
+            BIOME_MOD.MOUNTAINS: 1
+        },
+        MAIN_BIOME.WATER: {
+            BIOME_MOD.NONE: 2,
+            BIOME_MOD.FOREST: 2,
+            BIOME_MOD.SWAMP: 2,
+            BIOME_MOD.HILLS: 2,
+            BIOME_MOD.MOUNTAINS: 2
+        }
+    }
 
+    CITY_TYPE_FOOD_MOD = {
+        CITY_TYPE.GENERIC: [2, 2, 1, 1, 0],
+        CITY_TYPE.MANA: [1, 0],
+        CITY_TYPE.FARM: [3, 5],
+        CITY_TYPE.LIBRARY: [1, 1],
+        CITY_TYPE.FORT: [0, 0],
+        CITY_TYPE.FACTORY: [0, 0],
+        CITY_TYPE.MINE: [1, 0]
+    }
 
+    HOUSING = {
+        CITY_TYPE.GENERIC: [5, 7, 9, 11, 15],
+        CITY_TYPE.MANA: [3, 5],
+        CITY_TYPE.FARM: [3, 5],
+        CITY_TYPE.LIBRARY: [3, 5],
+        CITY_TYPE.FORT: [3, 5],
+        CITY_TYPE.FACTORY: [3, 5],
+        CITY_TYPE.MINE: [3, 5]
+    }
 
+    RESOURCE_DISTRIBUTION = {
+        MAIN_BIOME.PLAIN: {
+            '': [RESOURCE_TYPE.IRON, RESOURCE_TYPE.QUARTZ, RESOURCE_TYPE.WYVERNS],
+            BIOME_MOD.FOREST: [RESOURCE_TYPE.AMBER],
+            BIOME_MOD.SWAMP: [RESOURCE_TYPE.IRON],
+            BIOME_MOD.HILLS: [RESOURCE_TYPE.IRON, RESOURCE_TYPE.RUBIES, RESOURCE_TYPE.SAPHIRES, RESOURCE_TYPE.SILVER,
+                              RESOURCE_TYPE.GOLD],
+            BIOME_MOD.MOUNTAINS: [RESOURCE_TYPE.ALUMINIUM, RESOURCE_TYPE.GOLD, RESOURCE_TYPE.OBSIDIAN,
+                                  RESOURCE_TYPE.AMETHISTS]
+        },
+        MAIN_BIOME.DESERT: {
+            '': [RESOURCE_TYPE.HORSES],
+            BIOME_MOD.FOREST: [RESOURCE_TYPE.AMBER],
+            BIOME_MOD.SWAMP: [RESOURCE_TYPE.SAPHIRES],
+            BIOME_MOD.HILLS: [RESOURCE_TYPE.IRON, RESOURCE_TYPE.EMERALD, RESOURCE_TYPE.AMETHISTS, RESOURCE_TYPE.SILVER],
+            BIOME_MOD.MOUNTAINS: [RESOURCE_TYPE.ALUMINIUM, RESOURCE_TYPE.SILVER, RESOURCE_TYPE.DIAMONDS,
+                                  RESOURCE_TYPE.SAPHIRES]
+        }
+    }
 
+    BASE_FOOD_PRODUCTION = 2
--- world/models.py
+++ world/models.py
@@ -1,50 +1,54 @@
 from django.db import models
 
 from django.dispatch import receiver
-from .constants import RESOURCE_TYPE, MAIN_BIOME, BIOME_MOD, CITY_TYPE, POP_RACE, UNIQUE_TAGS, CELL_TAG_NAMES, POP_TAG_NAMES
+from .constants import RESOURCE_TYPE, MAIN_BIOME, BIOME_MOD, CITY_TYPE, POP_RACE, UNIQUE_TAGS, CELL_TAG_NAMES, \
+    POP_TAG_NAMES
 from .strings import month_names
 
-
+import json
 
 from django.utils.text import format_lazy
+
 # Create your models here.
 
 
 WORLD_END_CHOICES = [
-    ('LS','Мир уничтожен'),
-    ('WN','Мир спасён'),
-    ('NY','Исход не решён')
+    ('LS', 'Мир уничтожен'),
+    ('WN', 'Мир спасён'),
+    ('NY', 'Исход не решён')
 ]
 
 
 class World(models.Model):
     start_date = models.DateTimeField('Мир запущен')
-    end_date = models.DateTimeField('Мир закрыт', null=True, blank = True)
+    end_date = models.DateTimeField('Мир закрыт', null=True, blank=True)
     end_type = models.CharField(
-        max_length = 2,
-        choices = WORLD_END_CHOICES,
-        default = 'NY'
+        max_length=2,
+        choices=WORLD_END_CHOICES,
+        default='NY'
     )
-    is_active = models.BooleanField(default = False)
+    is_active = models.BooleanField(default=False)
     width = models.IntegerField()
     height = models.IntegerField()
-    ticks_age = models.IntegerField(default = 0)
+    ticks_age = models.IntegerField(default=0)
+
     def save(self, *args, **kwargs):
         if self.is_active:
             try:
-                temp = World.objects.get(is_active = True)
-                if self!=temp:
+                temp = World.objects.get(is_active=True)
+                if self != temp:
                     temp.is_active = False
                     temp.save()
             except World.DoesNotExist:
                 pass
         super(World, self).save(*args, **kwargs)
+
     @property
     def ticks_human_readable(self):
-        months = 11 - (self.ticks_age%12) if self.ticks_age <0 else self.ticks_age%12
-        years = self.ticks_age//12
-        return format_lazy( '{years}, {month}',
-        years=years, month = month_names[months])
+        months = 11 - (self.ticks_age % 12) if self.ticks_age < 0 else self.ticks_age % 12
+        years = self.ticks_age // 12
+        return format_lazy('{years}, {month}',
+                           years=years, month=month_names[months])
 
     def __getitem__(self, value):
         class Column:
@@ -60,48 +64,51 @@ class World(models.Model):
 class Cell(models.Model):
     x = models.IntegerField()
     y = models.IntegerField()
-    world  = models.ForeignKey(World, on_delete=models.CASCADE)
+    world = models.ForeignKey(World, on_delete=models.CASCADE)
     main_biome = models.CharField(
-        choices = MAIN_BIOME.choices,
+        choices=MAIN_BIOME.choices,
         max_length=3
     )
     biome_mod = models.CharField(
-        choices = BIOME_MOD.choices,
+        choices=BIOME_MOD.choices,
         max_length=3
     )
     city_type = models.CharField(
-        choices = CITY_TYPE.choices,
-        max_length = 3
+        choices=CITY_TYPE.choices,
+        max_length=3
     )
-    city_tier = models.IntegerField(default = 0)
+    city_tier = models.IntegerField(default=0)
     local_resource = models.CharField(
-        choices = RESOURCE_TYPE.choices,
-        max_length = 3, blank = True
+        choices=RESOURCE_TYPE.choices,
+        max_length=3, blank=True
     )
 
     @property
     def name(self):
         try:
-            tag = self.tags.get(name = CELL_TAG_NAMES.NAME)
+            tag = self.tags.get(name=CELL_TAG_NAMES.NAME)
             return tag.content
         except CellTag.DoesNotExist:
             return f'{self.x}x{self.y}'
 
     @name.setter
     def name(self, value):
-        tag, created = self.tags.get_or_create(name = CELL_TAG_NAMES.NAME)
+        tag, created = self.tags.get_or_create(name=CELL_TAG_NAMES.NAME)
         tag.content = value
         tag.save()
-
 
     @property
     def factions(self):
         faction_tags = PopTag.objects.filter(
-            pop__location = self,
-            name = POP_TAG_NAMES.FACTION
+            pop__location=self,
+            name=POP_TAG_NAMES.FACTION
         )
         faction_ids = [int(tag.content) for tag in faction_tags]
-        return Faction.objects.filter(id__in = faction_ids)
+        return Faction.objects.filter(id__in=faction_ids)
+	@property
+	def boosts(self):
+		boost_tags = self.tags.filter(name=CELL_TAG_NAMES.BOOST)
+		return boost_tags
 
     def __repr__(self):
         return f'<Cell ({self.x};{self.y}) world_id={self.world.id}>'
@@ -110,22 +117,25 @@ class Cell(models.Model):
         return f'Cell {self.x}x{self.y}'
 
     class Meta:
-        ordering = ['world','x','y']
+        ordering = ['world', 'x', 'y']
+
 
 class Pop(models.Model):
     location = models.ForeignKey(Cell, on_delete=models.CASCADE)
     race = models.CharField(
-        choices = POP_RACE.choices,
-        max_length = 3
+        choices=POP_RACE.choices,
+        max_length=3
     )
+
     @property
     def growth(self):
-        tag, created = self.tags.get_or_create(name = POP_TAG_NAMES.GROWTH, defaults = {'content': '0'})
+        tag, created = self.tags.get_or_create(name=POP_TAG_NAMES.GROWTH, defaults={'content': '0'})
         return int(tag.content)
+
     @growth.setter
     def growth(self, value):
         assert isinstance(value, int)
-        tag, created = self.tags.get_or_create(name = POP_TAG_NAMES.GROWTH, defaults = {'content': '0'})
+        tag, created = self.tags.get_or_create(name=POP_TAG_NAMES.GROWTH, defaults={'content': '0'})
         tag.content = str(value)
         tag.save()
 
@@ -133,26 +143,26 @@ class Pop(models.Model):
     def faction(self):
         tag = None
         try:
-            tag = self.tags.get(name = POP_TAG_NAMES.FACTION)
+            tag = self.tags.get(name=POP_TAG_NAMES.FACTION)
             result = Faction.objects.get(pk=int(tag.content))
             return result
         except PopTag.DoesNotExist:
             return None
         except Faction.DoesNotexist:
-           tag.delete()
-           return None
+            tag.delete()
+            return None
 
     @faction.setter
     def faction(self, value):
         if value is None:
             try:
-                tag = self.tags.get(name = POP_TAG_NAMES.FACTION)
+                tag = self.tags.get(name=POP_TAG_NAMES.FACTION)
                 tag.delete()
             except PopTag.DoesNotExist:
                 pass
         else:
             assert isinstance(value, Faction)
-            tag, created = self.tags.get_or_create(name = POP_TAG_NAMES.GROWTH)
+            tag, created = self.tags.get_or_create(name=POP_TAG_NAMES.GROWTH)
             tag.content = f'{value.id}'
             tag.save()
 
@@ -161,36 +171,48 @@ class Pop(models.Model):
         from players.models import Character
         tag = None
         try:
-            tag = self.tags.get(name = POP_TAG_NAMES.FACTION)
+            tag = self.tags.get(name=POP_TAG_NAMES.FACTION)
             result = Character.objects.get(pk=int(tag.content))
             return result
         except PopTag.DoesNotExist:
             return None
         except Character.DoesNotexist:
-           tag.delete()
-           return None
+            tag.delete()
+            return None
 
+	@property
+	def tied_character(self):
+		from players.models import CharTag
+		from players.constants import CHAR_TAG_NAMES
+		try:
+			tag = CharTag.objects.get(
+				name=CHAR_TAG_NAMES.TIED_POP,
+				content=f'{self.id}'
+			)
+			return tag.character
+		except CharTag.DoesNotExist:
+			return None
 
 
 class Faction(models.Model):
-    name = models.CharField(max_length = 100, blank = True)
+    name = models.CharField(max_length=100, blank=True)
 
     @property
     def pops(self):
         return Pop.objects.filter(
-            tags__name = POP_TAG_NAMES.FACTION,
-            tags__content = f'{self.id}'
+            tags__name=POP_TAG_NAMES.FACTION,
+            tags__content=f'{self.id}'
         )
 
+
 class FactionMember(models.Model):
-    faction = models.ForeignKey('Faction', on_delete=models.CASCADE, related_name = "members")
-    character = models.ForeignKey('players.Character', on_delete=models.CASCADE, related_name = "factions")
+    faction = models.ForeignKey('Faction', on_delete=models.CASCADE, related_name="members")
+    character = models.ForeignKey('players.Character', on_delete=models.CASCADE, related_name="factions")
     is_leader = models.BooleanField()
     can_build = models.BooleanField()
     can_use_army = models.BooleanField()
     can_recruit = models.BooleanField()
-    title_name = models.CharField(max_length = 100, blank = True)
-
+    title_name = models.CharField(max_length=100, blank=True)
 
 
 @receiver(models.signals.post_save, sender=Faction)
@@ -199,14 +221,16 @@ def default_name(sender, instance, created, **kwargs):
         instance.name = f'Faction {instance.id}'
         instance.save()
 
+
 class CellTag(models.Model):
-    cell = models.ForeignKey(Cell, on_delete=models.CASCADE, related_name = "tags")
-    name = models.CharField(max_length=16, choices = CELL_TAG_NAMES.choices)
-    content = models.CharField(max_length = 100)
+    cell = models.ForeignKey(Cell, on_delete=models.CASCADE, related_name="tags")
+    name = models.CharField(max_length=16, choices=CELL_TAG_NAMES.choices)
+    content = models.CharField(max_length=100)
+
     def save(self, *args, **kwargs):
         if self.name in UNIQUE_TAGS.ONE_PER_CELL:
             try:
-                todel = CellTag.objects.get(name = self.name, content = self.content)
+                todel = CellTag.objects.get(name=self.name, content=self.content)
                 todel.delete()
             except CellTag.DoesNotExist:
                 pass
@@ -214,13 +238,14 @@ class CellTag(models.Model):
 
 
 class PopTag(models.Model):
-    pop = models.ForeignKey(Pop, on_delete=models.CASCADE, related_name = "tags")
-    name = models.CharField(max_length=16, choices = POP_TAG_NAMES.choices)
-    content = models.CharField(max_length = 100)
+    pop = models.ForeignKey(Pop, on_delete=models.CASCADE, related_name="tags")
+    name = models.CharField(max_length=16, choices=POP_TAG_NAMES.choices)
+    content = models.CharField(max_length=100)
+
     def save(self, *args, **kwargs):
         if self.name in UNIQUE_TAGS.ONE_PER_POP:
             try:
-                todel = PopTag.objects.get(name = self.name, content = self.content)
+                todel = PopTag.objects.get(name=self.name, content=self.content)
                 todel.delete()
             except PopTag.DoesNotExist:
                 pass
@@ -228,24 +253,27 @@ class PopTag(models.Model):
 
 
 class CellRenameRequest(models.Model):
-    cell = models.ForeignKey(Cell, on_delete=models.CASCADE, related_name = "renames")
-    new_name = models.CharField(max_length = 100)
+    cell = models.ForeignKey(Cell, on_delete=models.CASCADE, related_name="renames")
+    new_name = models.CharField(max_length=100)
     player = models.ForeignKey('players.Player', on_delete=models.CASCADE)
+
     def approve(self):
         self.cell.name = self.new_name
         self.delete()
+
     def reject(self):
         self.delete()
 
+
 class FactionRenameRequest(models.Model):
-    new_name = models.CharField(max_length = 100)
-    faction = models.ForeignKey('Faction', on_delete=models.CASCADE, related_name = "renames")
+    new_name = models.CharField(max_length=100)
+    faction = models.ForeignKey('Faction', on_delete=models.CASCADE, related_name="renames")
     player = models.ForeignKey('players.Player', on_delete=models.CASCADE)
+
     def approve(self):
         self.faction.name = self.new_name
         self.faction.save()
         self.delete()
+
     def reject(self):
         self.delete()
-
-
